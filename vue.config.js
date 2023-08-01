6a478fe18f6dce544bbd1df3f01702934c294575
Yudi <yudi.mukhyiddin@ecampus.ut.ac.id>
2023-08-01 10:49:41 +0700

    up

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'development' ? '/portofolio/' : '/'
-  }  
+import { defineConfig } from "vite";
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  base:'/portofolio/',
+  plugins:[vue()]
+})
