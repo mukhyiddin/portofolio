8a46edd70074459f06d444b9ee76cbc35b882ff3
Yudi <yudi.mukhyiddin@ecampus.ut.ac.id>
2023-08-04 14:27:50 +0700

    COC

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,24 @@
-// vue.config.js file to be placed in the root of your repository
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/portofolio/'
-      : '/'
-  }
+export const publicPath = process.env.NODE_ENV === 'production'
+  ? '/portofolio/'
+  : '/';
+export const outputDir = 'dist';
+export const pages = {
+  index: {
+    entry: 'src/main.js',
+    template: 'public/index.html',
+    filename: 'index.html',
+  },
+  // Add your other pages here if you have multiple pages
+};
+export const configureWebpack = {
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public/404.html',
+          to: '404.html',
+        },
+      ],
+    }),
+  ],
+};
